865244b0b6edbb3eb08da94bd5ced4b59b100b21
Харитонов Дмитрий <hdi@programstore.ru>
2023-04-17 11:13:09 +0400

    Заготовка Readme + Код

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string[] arr = new string[4] {"adfasddf","sr","sdfs","456456"};
+string[] res = new string[0];
+for (int i = 0; i<4; i++){    
+    if(arr[i].Length < 3)
+    {
+        int length = arr.Length + 1;
+        Array.Resize(ref res, length);  
+        res[length-1] =  arr[i]; 
+    }        
+}
+
+for (int j=0; j<res.Length; j++)
+{
+    Console.WriteLine(res[j]);
+}
